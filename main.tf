f4f830bf651cf4812d5b43d9a934baae6c3a9f20
Billie Thompson <billie.thompson@armakuni.com>
2023-09-26 11:20:21 +0200

    feat: Allow refs to be pattern matched
    This is to enable automated dependency bumping
    
    Co-authored-by: Tom Oram <tom.oram@armakuni.com>

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role" "this" {
       {
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
-          StringEquals = {
+          StringLike = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
             "token.actions.githubusercontent.com:sub" = "repo:${var.github_owner}/${var.github_repository}:ref:${var.git_ref}"
           }
